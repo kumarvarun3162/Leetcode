e1877b06e80876b65ac6ff34dbeb057b74f529ef
VARUN KUMAR <kumarvarun3162@gmail.com>
2026-07-26 15:56:42 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.25%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n <= 3) {
+            return true;
+        }
+        else if (n % 4 == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+};
